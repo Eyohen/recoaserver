0c7135353a4027763b5b98249759369e07e888df
Eyohen <henry.eyo2@gmail.com>
2023-12-06 19:12:36 +0100

    change cors configuration

--- index.js
+++ index.js
@@ -31,7 +31,10 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:["http://localhost:5173","http://localhost:5174"],
+// app.use(cors({origin:["http://localhost:5173","http://localhost:5174"],
+// credentials:true
+// }))
+app.use(cors({origin:"https://sore-jade-shrimp-boot.cyclic.app",
 credentials:true
 }))
 //app.use(cors())
