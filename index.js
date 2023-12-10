461a23cbd44e2db2c170439704e9c3891e27ebd9
Eyohen <henry.eyo2@gmail.com>
2023-12-10 19:53:39 +0100

    changed cors url

--- index.js
+++ index.js
@@ -31,12 +31,12 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:["http://localhost:5173","http://localhost:5174"],
-credentials:true
-}))
-// app.use(cors({origin:"http://localhost:5173",
+// app.use(cors({origin:["http://localhost:5173","http://localhost:5174"],
 // credentials:true
 // }))
+app.use(cors({origin:"https://recoaproject.vercel.app",
+credentials:true
+}))
 // app.use(cors())
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
