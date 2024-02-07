2d11e0a821932f3cea3181538f60288af92fc28a
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-02-07 01:50:55 +0100

    Refactor community routes and controllers

--- routes/community.js
+++ routes/community.js
@@ -1,123 +1,33 @@
 const express=require('express')
 const router=express.Router()
-const User=require('../models/User')
-const bcrypt=require('bcrypt')
-const Community =require('../models/Community')
-
-
+const {
+    CreateCommunity,
+    UpdateCommunity,
+    DeleteCommunity,
+    GetCommunity,
+    SearchCommunity,
+    GetUserCommunity
+} = require('../controllers/community')
 // const Comment=require('../models/Comment')
-const verifyToken = require('../verifyToken')
-const SubMarket = require('../models/SubMarket')
+const verifyToken = require('../middlewares/verifyToken')
 
 //CREATE
-router.post("/create",verifyToken,async (req,res)=>{
-    try{
-            
-        const submarket = await SubMarket.findById(req.body.submarket)
-     
-
-        if (!submarket) {
-          console.log(`Submarket not found.`);
-          return null;
-        }
-
-        const newCommunity =new Community(req.body)
-    
-        const savedCommunity = await newCommunity.save()
-    
-        // const Communitys = await Community.create(CommunityDataArray);
-    
-    
-        submarket.communities = submarket.communities.concat(savedCommunity);
-    
-        await submarket.save();
-    
-        res.status(200).json(savedCommunity)
-    }
-    catch(err){
-        
-        res.status(500).json({message:"Community not created"})
-    }
-     
-})
-
-
+router.post("/create",verifyToken,CreateCommunity)
 
 //UPDATE
-router.put("/:id",verifyToken,async (req,res)=>{
-    try{
-       
-        const updatedCommunity =await Community.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
-        res.status(200).json(updatedCommunity)
-
-    }
-    catch(err){
-        console.log(err.message)
-        res.status(500).json(err)
-    }
-})
-
-
+router.put("/:id",verifyToken,UpdateCommunity)
 
 //DELETE
-router.delete("/:id",verifyToken,async (req,res)=>{
-    try{
-        await Community.findByIdAndDelete(req.params.id)
-        // await Comment.deleteMany({postId:req.params.id})
-        res.status(200).json("Community has been deleted!")
-
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
-
+router.delete("/:id",verifyToken,DeleteCommunity)
 
 //GET Community
-router.get("/:id",async (req,res)=>{
-    // console.log(req.params.id)
-    try{
-        const Community =await Community.findById(req.params.id).populate('submarket').populate('unitTypes')
-        // console.log(Community)
-        if(!Community){
-            throw Error
-        }
-        
-        res.status(200).json(Community)
-
-    
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
+router.get("/:id",GetCommunity)
 
 //GET Communitys
-router.get("/", async (req,res)=>{
-    const query=req.query
-    
-    try{
-        const searchFilter={
-            title:{$regex:query.search, $options:"i"}
-        }
-        const Communitys = await Community.find(query.search?searchFilter:null).populate('submarket').populate('unitTypes')
-        res.status(200).json(Communitys)
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
+router.get("/", SearchCommunity)
 
 //GET USER POSTS
-router.get("/user/:userId",async (req,res)=>{
-    try{
-        const Communitys =await Community.find({userId:req.params.userId})
-        res.status(200).json(Communitys)
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
+router.get("/user/:userId",GetUserCommunity)
 
 
 
