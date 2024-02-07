a893834c2bb4175e68c2565e0e74e6a3e76449f8
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-02-07 01:51:37 +0100

    Update user routes and controllers

--- routes/users.js
+++ routes/users.js
@@ -4,69 +4,28 @@ const User=require('../models/User')
 const bcrypt=require('bcrypt')
 const Community =require('../models/Community')
 // const Comment=require('../models/Comment')
-const verifyToken = require('../verifyToken')
+const verifyToken = require('../middlewares/verifyToken')
+const {
+    UpdateUser,
+    DeleteUser,
+    GetUser,
+    SearchUsers,
+} = require('../controllers/users')
 
 
 //UPDATE
-router.put("/:id",verifyToken,async (req,res)=>{
-    try{
-        if(req.body.password){
-            const salt = await bcrypt.genSalt(10)
-            req.body.password = await bcrypt.hashSync(req.body.password,salt)
-        }
-        console.log(req.body)
-        const updatedUser=await User.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
-        res.status(200).json(updatedUser)
-
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
+router.put("/:id",verifyToken,UpdateUser)
 
 
 //DELETE
-router.delete("/:id",verifyToken,async (req,res)=>{
-    try{
-        await User.findByIdAndDelete(req.params.id)
-        await Apartment.deleteMany({userId:req.params.id})
-        await Comment.deleteMany({userId:req.params.id})
-        res.status(200).json("User has been deleted!")
-
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
+router.delete("/:id",verifyToken,DeleteUser)
 
 //GET USERS
-router.get("/",async (req,res)=>{
-    const query=req.query
-    
-    try{
-        const searchFilter={
-            title:{$regex:query.search, $options:"i"}
-        }
-        const users = await User.find(query.search?searchFilter:null)
-        res.status(200).json(users)
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
+router.get("/",SearchUsers)
 
 
 //GET USER
-router.get("/:id",async (req,res)=>{
-    try{
-        const user=await User.findById(req.params.id)
-        const {password,...info}=user._doc
-        res.status(200).json(info)
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
+router.get("/:id",GetUser)
 
 
 module.exports=router
