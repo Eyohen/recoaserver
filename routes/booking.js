2ff8de87fc90a36b6039486c3bd99f3923615aff
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-02-07 01:50:47 +0100

    Update booking routes and controllers

--- routes/booking.js
+++ routes/booking.js
@@ -5,78 +5,35 @@ const bcrypt=require('bcrypt')
 const Booking = require('../models/Booking')
 const SubMarket =require('../models/SubMarket')
 const Community =require('../models/Community')
-const verifyToken = require('../verifyToken')
+const verifyToken = require('../middlewares/verifyToken')
+const {
+    CreateBooking,
+    UpdateBooking,
+    DeleteBooking,
+    GetBooking,
+    SearchBookings,
+    GetUserBooking
+} = require('../controllers/booking')
+
 
 //CREATE
 
 
 //UPDATE
-router.put("/:id",verifyToken,async (req,res)=>{
-    try{
-       
-        const updatedBooking =await Booking.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
-        res.status(200).json(updatedBooking)
-
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
+router.put("/:id",verifyToken,UpdateBooking)
 
 
 //DELETE
-router.delete("/:id",verifyToken,async (req,res)=>{
-    try{
-        await Booking.findByIdAndDelete(req.params.id)
-        // await Comment.deleteMany({postId:req.params.id})
-        res.status(200).json("Booking has been deleted!")
-
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
+router.delete("/:id",verifyToken,DeleteBooking)
 
 
 //GET Estate DETAILS
-router.get("/:id",async (req,res)=>{
-    try{
-        const booking =await Booking.findById(req.params.id).populate('communities')
-        res.status(200).json(booking)
-       
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
+router.get("/:id",GetBooking)
 
 //GET Estates
-router.get("/",async (req,res)=>{
-    const query=req.query
-    
-    try{
-        const searchFilter={
-            title:{$regex:query.search, $options:"i"}
-        }
-        const booking =await Booking.find(query.search?searchFilter:null).populate('communities')
-        res.status(200).json(booking)
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
+router.get("/",SearchBookings)
 
 //GET USER POSTS
-router.get("/user/:userId",async (req,res)=>{
-    try{
-        const booking =await Booking.find({userId:req.params.userId})
-        res.status(200).json(booking)
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
-
-
+router.get("/user/:userId",GetUserBooking)
 
 module.exports=router
