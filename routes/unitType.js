c6c8ad7664c703636e46ed5feea51fa359aae887
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-02-07 01:51:30 +0100

    Refactor unitType.js routes

--- routes/unitType.js
+++ routes/unitType.js
@@ -4,122 +4,31 @@ const User=require('../models/User')
 const bcrypt=require('bcrypt')
 const Community =require('../models/Community')
 const UnitType =require('../models/UnitType')
-
-// community = UnitType
+const {
+    CreateUnitType,
+    UpdateUnitType,
+    DeleteUnitType,
+    GetUnitType,
+    FindUnitType,
+} = require('../controllers/unitType')
 
 // const Comment=require('../models/Comment')
-const verifyToken = require('../verifyToken')
-
+const verifyToken = require('../middlewares/verifyToken')
 
 //CREATE
-router.post("/create",verifyToken,async (req,res)=>{
-    try{
-            
-        const community = await Community.findById(req.body.community)
-     
-        if (!community) {
-          console.log(`Community not found.`);
-          return null;
-        }
-
-        const newUnitType = new UnitType(req.body)
-    
-        const savedUnitType = await newUnitType.save()
-    
-        
-    
-    
-        community.unitTypes = community.unitTypes.concat(savedUnitType);
-    
-        await community.save();
-    
-        res.status(200).json(savedUnitType)
-    }
-    catch(err){
-        
-        res.status(500).json({message:"UnitType not created"})
-    }
-     
-})
-
-
+router.post("/create",verifyToken,CreateUnitType)
 
 //UPDATE
-router.put("/:id",verifyToken,async (req,res)=>{
-    try{
-       
-        const updatedType =await UnitType.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
-        res.status(200).json(updatedType)
-
-    }
-    catch(err){
-        console.log(err.message)
-        res.status(500).json(err)
-    }
-})
-
-
+router.put("/:id",verifyToken,UpdateUnitType)
 
 //DELETE
-router.delete("/:id",verifyToken,async (req,res)=>{
-    try{
-        await UnitType.findByIdAndDelete(req.params.id)
-        // await Comment.deleteMany({postId:req.params.id})
-        res.status(200).json({message:"Community has been deleted!"})
-
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
-
+router.delete("/:id",verifyToken,DeleteUnitType)
 
 //GET UnitType
-router.get("/:id",async (req,res)=>{
-    // console.log(req.params.id)
-    try{
-        const unitType = await UnitType.findById(req.params.id).populate('community')
-        // console.log(Community)
-        // if(!Community){
-        //     throw Error
-        // }
-        
-        res.status(200).json(unitType)
-
-    
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
+router.get("/:id",GetUnitType)
 
 //GET UnitTypes
-router.get("/", async (req,res)=>{
-    const query = req.query
-    
-    try{
-        const searchFilter={
-            title:{$regex:query.search, $options:"i"}
-        }
-        const unitTypes = await UnitType.find(query.search?searchFilter:null).populate('community')
-        res.status(200).json(unitTypes)
-    }
-    catch(err){
-        res.status(500).json(err)
-    }
-})
-
-//GET USER POSTS
-// router.get("/user/:userId",async (req,res)=>{
-//     try{
-//         const Communitys =await Community.find({userId:req.params.userId})
-//         res.status(200).json(Communitys)
-//     }
-//     catch(err){
-//         res.status(500).json(err)
-//     }
-// })
-
+router.get("/", FindUnitType)
 
 
 module.exports=router
