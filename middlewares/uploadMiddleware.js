231676e7d7f129b1f63844a8876eba6c2f99e5f1
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-02-07 01:49:25 +0100

    Add upload middleware for handling file uploads

---
+++ middlewares/uploadMiddleware.js
@@ -0,0 +1,25 @@
+const multer = require('multer');
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype.startsWith('image/') || // Images
+        file.mimetype.startsWith('video/')  // Videos
+        // file.mimetype === 'application/pdf' || // PDFs
+        // file.mimetype === 'text/csv' || // CSVs
+        // file.mimetype === 'application/vnd.ms-excel' // Excel
+    ) {
+        cb(null, true);
+    } else {
+        cb(new Error('Please upload only images, or videos.'), false);
+    }
+};
+
+const storage = multer.memoryStorage(); // Store file in memory
+
+const uploadFile = multer({
+    storage: storage,
+    limits: { fileSize: 1024 * 1024 * 5 }, // set max file size to 5MB
+    fileFilter: fileFilter,
+});
+
+module.exports = uploadFile;
