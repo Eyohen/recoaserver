2f8b9ab8d4a3e7ff8b4c5fe0b44bed52f372a7cb
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-02-08 21:12:18 +0100

    Refactor reservation routes

--- routes/reservation.js
+++ routes/reservation.js
@@ -1,28 +1,28 @@
 const express = require('express');
 const router = express.Router();
-const { Reservation } = require('../models/Reservation');
 const {
     newReservation,
     getReservations,
     getreservation,
-    upateReservation,
+    updateReservation,
     deleteReservation
 } = require('../controllers/reservation');
 const verifyToken = require('../middlewares/verifyToken')
 
 
 // Create a Reservation
-router.post('/reservation', verifyToken, newReservation);
+router.post('/create', verifyToken, newReservation);
 
-// Get All reservation
-router.get('/reservation', getReservations);
+// Get All 
+router.get('/', getReservations);
 
 // Get a Reservation by ID
-router.get('/reservation/:id', getreservation);
+router.get('/:id', getreservation);
 
 // Update a Reservation
-router.put('/reservation/:id', verifyToken, upateReservation);
+router.put('/:id', verifyToken, updateReservation);
+
 // Delete a Reservation
-router.delete('/reservation/:id', verifyToken, deleteReservation);
+router.delete('/:id', verifyToken, deleteReservation);
 
 module.exports = router;
