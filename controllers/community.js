f04e3539ca4a81cd2880287cf20c852e2e052c97
Emmanuel Umukoro <emmanuel.umukoro22@gmail.com>
2024-02-08 18:46:01 +0100

    Refactor community controller

--- controllers/community.js
+++ controllers/community.js
@@ -1,57 +1,55 @@
-const express=require('express')
-const router=express.Router()
-const User=require('../models/User')
-const bcrypt=require('bcrypt')
-const Community =require('../models/Community')
-
-
+const express = require('express')
+const router = express.Router()
+const User = require('../models/User')
+const bcrypt = require('bcrypt')
+const Community = require('../models/Community')
 // const Comment=require('../models/Comment')
 const verifyToken = require('../middlewares/verifyToken')
 const SubMarket = require('../models/SubMarket')
 
 //CREATE
-const CreateCommunity = async (req,res)=>{
-    try{
-            
+const CreateCommunity = async (req, res) => {
+    try {
+
         const submarket = await SubMarket.findById(req.body.submarket)
-     
+
 
         if (!submarket) {
-          res.status(404).json({ message: "Submarket not found" });
-          return null;
+            res.status(404).json({ message: "Submarket not found" });
+            return null;
         }
 
-        const newCommunity =new Community(req.body)
-    
+        const newCommunity = new Community(req.body)
+
         const savedCommunity = await newCommunity.save()
-    
+
         // const Communitys = await Community.create(CommunityDataArray);
-    
-    
+
+
         submarket.communities = submarket.communities.concat(savedCommunity);
-    
+
         await submarket.save();
-    
+
         res.status(200).json(savedCommunity)
     }
-    catch(err){
-        
-        res.status(500).json({message:"Community not created"})
+    catch (err) {
+
+        res.status(500).json({ message: "Community not created" })
     }
-     
+
 }
 
 
 
 //UPDATE
-const UpdateCommunity = async (req,res)=>{
-    try{
-       
-        const updatedCommunity =await Community.findByIdAndUpdate(req.params.id,{$set:req.body},{new:true})
+const UpdateCommunity = async (req, res) => {
+    try {
+
+        const updatedCommunity = await Community.findByIdAndUpdate(req.params.id, { $set: req.body }, { new: true })
         res.status(200).json(updatedCommunity)
 
     }
-    catch(err){
+    catch (err) {
         console.log(err.message)
         res.status(500).json(err)
     }
@@ -60,71 +58,71 @@ const UpdateCommunity = async (req,res)=>{
 
 
 //DELETE
-const DeleteCommunity = async (req,res)=>{
-    try{
+const DeleteCommunity = async (req, res) => {
+    try {
         await Community.findByIdAndDelete(req.params.id)
         // await Comment.deleteMany({postId:req.params.id})
         res.status(200).json("Community has been deleted!")
 
     }
-    catch(err){
+    catch (err) {
         res.status(500).json(err)
     }
 }
 
 
 //GET Community
-const GetCommunity = async (req,res)=>{
+const GetCommunity = async (req, res) => {
     // console.log(req.params.id)
-    try{
-        const foundCommunity =await Community.findById(req.params.id).populate('submarket').populate('unitTypes')
+    try {
+        const foundCommunity = await Community.findById(req.params.id).populate('submarket').populate('unitTypes')
         console.log(foundCommunity)
         // console.log(Community)
-        if(!Community){
+        if (!Community) {
             res.status(404).json("Community not found!")
         }
-        
+
         res.status(200).json(foundCommunity)
 
-    
+
     }
-    catch(err){
+    catch (err) {
         console.log(err)
         res.status(500).json(err)
     }
 }
 
 //GET Communitys
-const SearchCommunity =  async (req,res)=>{
-    const query=req.query
-    
-    try{
-        const searchFilter={
-            title:{$regex:query.search, $options:"i"}
+const SearchCommunity = async (req, res) => {
+    const query = req.query
+
+    try {
+        const searchFilter = {
+            title: { $regex: query.search, $options: "i" }
         }
-        const Communitys = await Community.find(query.search?searchFilter:null).populate('submarket').populate('unitTypes')
+        const Communitys = await Community.find(query.search ? searchFilter : null).populate('submarket').populate('unitTypes')
         res.status(200).json(Communitys)
     }
-    catch(err){
+    catch (err) {
         console.log(err)
         res.status(500).json(err)
     }
 }
 
 //GET USER POSTS
-const GetUserCommunity = async (req,res)=>{
-    try{
-        const Communitys =await Community.find({userId:req.params.userId})
+const GetUserCommunity = async (req, res) => {
+    try {
+        const Communitys = await Community.find({ userId: req.params.userId })
         res.status(200).json(Communitys)
     }
-    catch(err){
+    catch (err) {
         res.status(500).json(err)
     }
 }
 
 
 
-module.exports={
+module.exports = {
     CreateCommunity,
     UpdateCommunity,
     DeleteCommunity,
